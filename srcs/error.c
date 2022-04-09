b26cda803f15f9fa51ceb11851b05c9eba80e658
Minguk Gaang <minguk.gaang@gmail.com>
2022-04-09 21:31:37 +0200

    trying is_surrounded func, encountered a pb with bool**map because of the space character

--- srcs/error.c
+++ srcs/error.c
@@ -6,17 +6,34 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 13:08:30 by min-kang          #+#    #+#             */
-/*   Updated: 2022/04/09 14:21:30 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/04/09 21:30:41 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-int	is_surrounded(char **map)
+int	is_surrounded(bool **map)
 {
-	int	i;
+	int		i;
+	int		j;
+	bool	open;
 
-	
-	/* 가로 세로 다르게
-	/* should be the wall when if index 0 and last
+	//** tricky: there can be a line only with space. Should deal with those cases before coming into this function scope.
+	//** maybe space value should be TRUE as a bool value.
+	//** it's trickier... (because of the space character)
+	while (map[i])
+	{
+		j = -1;
+		open = false;
+		while (map[i][++j])
+		{
+			if (!open && map[i][j] == true)
+				open = true;
+			if (open && map[i][j] == false)
+				open = false;
+		}
+		if (open == true)
+			return (0);
+	}
+	return (1);
 }
