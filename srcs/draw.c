e05a484bcf0e7947892dce6306277eecfcc1bc2c
Minguk Gaang <minguk.gaang@gmail.com>
2022-04-14 18:25:08 +0200

    draw_raycast func done

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 19:32:18 by min-kang          #+#    #+#             */
-/*   Updated: 2022/04/14 11:59:26 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/04/14 18:24:51 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,32 +29,45 @@ double	anti_fisheye_distortion(double dist)
 	
 }
 
-void	draw_raycast(t_map map, int h)
+void	draw_raycast(t_map map, int h, int rayN)
 {
+	double	startX;
 	double	startY;
+	int		pixNbX;
+	int		i;
+	int		j;
 
+	pixNbX = startX / ANGLE;
+	startX = rayN * pixNbX;
 	startY = (SCREEN_Y - h) /2;
+	i = -1;
+	while (++i < pixNbX)
+	{
+		j = -1;
+		while (++j < h)
+			my_mlx_pixel_put(gui, startX + i, startY + j, 0xFFFF00);
+	}
 }
 
-void	raycast(t_map map, double angle)
+void	raycast(t_map map, double angle, int rayN)
 {
 	t_raycast	res;
+	double		r;
 
 	res = digital_differential_analyzer(map.pos, map.map2d, angle);
-	draw_raycast(map, get_hight(anti_fisheye_distortion(res.dist)));
+	draw_raycast(map, get_hight(anti_fisheye_distortion(res.dist)), rayN);
 }
 
 int draw(t_map map)
 {
-	double	r;
+	int		ray;
+	double	startAngle;
+	int		i;
 
-	raycast(map.pos, map.map2d, map.theta);
 	r = M_PI / 180;
-	while (r < ANGLE / 2)
-	{
-		raycast(map, map.theta - r);
-		raycast(map, map.theta + r);
-		r += M_PI / 180;
-	}
+	startAngle = map.theta - r * ANGLE / 2;
+	i = -1;
+	while (++i < ANGLE)
+		raycast(map, startAngle + r * i, i);
 	return (0);
 }
