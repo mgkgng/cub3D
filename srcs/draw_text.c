b0ad74531bde6996a0d368835f506da9f86c7c5e
Mathieu <math.lecherbonnier@gmail.com>
2022-05-02 21:19:08 +0200

    Save texture

--- srcs/draw_text.c
+++ srcs/draw_text.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   draw_text.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mlecherb <mlecherb@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/30 17:46:05 by mlecherb          #+#    #+#             */
-/*   Updated: 2022/04/30 18:52:36 by mlecherb         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "cub3D.h"
 
 // int	wich_texture(t_game *game, t_raycast ray)
@@ -17,19 +5,41 @@
 
 // }
 
+char	**get_texture(int fd)
+{
+	char	*line;
+	int		ret;
+	char	buf[2];
+
+	ret = read(fd, buf, 2);
+	buf[ret] = '\0';
+	line = ft_strdup(buf);
+	while (ret > 0)
+	{
+		ret = read(fd, buf, 2);
+		buf[ret] = '\0';
+		line = ft_strjoin(line, buf);
+		// free line dans strjoin
+	}
+	return (ft_split(line, '\n'));
+}
+
 void	draw_text(t_game *game, int h, int x, t_raycast ray)
 {
-	// int	fd;
-
-	// fd = open(game->draw.nswe[wich_texture(game, ray)], O_RDONLY);
 	// Essaye avec une texture partout
 	int	startY;
-	int	i;
 	int	line; // C'est la ligne de l'image qu'on doit afficher;
 
-	line = (int) ray.wall.x;
-	line = line * // Longueur longeur de l'image;
-	startY = (int) ((SCREEN_Y - h) /2);
-	i = 0;
-	
+	// line = (int) ray.wall.x;
+	// line = line * // Longueur longeur de l'image;
+	// startY = (int) ((SCREEN_Y - h) /2);
+	// Il faut trouver le line_start (h)
+	// Le debut de la ou il faut commencer a dessiner la ligne dans la fenetre.
+	// i - line_start = position de la ou il faut dessiner. (K)
+	// line_end - line_start = longueur de la ligne. (L)
+	// (K / L) * texture_hauteur = le pixel qui faut afficher.
+	// (h = LIGNE START)
+	/*
+		Probleme : Recupere la ligne de l'image.
+	*/
 }
