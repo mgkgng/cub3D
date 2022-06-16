e5b4795b3d9b3c5ba74beb5889672e0851697352
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-17 01:32:30 +0200

    feature/door: successfully showed doors, except we paint closer ones first

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 SRCS =	srcs/cub3D.c srcs/key.c srcs/main.c srcs/parse.c \
 		srcs/utils.c srcs/draw.c srcs/dda.c srcs/parse_utils.c \
-		bonus/minimap.c bonus/mouse.c bonus/sprite.c bonus/door.c\
+		bonus/minimap.c bonus/mouse.c bonus/door.c\
 		srcs/draw_text.c srcs/parse_draw.c srcs/parse_error.c srcs/parse_map.c\
 		srcs/key_management.c srcs/movement.c \
 
@@ -23,7 +23,7 @@ all:		${NAME}
 ${NAME}:	${OBJS}
 			make all -C ${LIB_PATH}
 			make all -C ${MLX_PATH}
-			gcc ${CFLAGS} -I${INCLUDES} -o ${NAME} ${OBJS} ${MLX} ./minilibx_opengl_20191021/libmlx.a ./libft/libft.a -fsanitize=address
+			gcc ${CFLAGS} -I${INCLUDES} -o ${NAME} ${OBJS} ${MLX} minilibx/libmlx.a ./libft/libft.a -fsanitize=address
 			
 .c.o:
 			gcc ${CFLAG} -I${INCLUDES} -c $< -o ${<:.c=.o}
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 23:13:58 by min-kang          #+#    #+#             */
-/*   Updated: 2022/06/16 23:53:25 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/06/17 01:03:11 by min-kang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,3 +36,22 @@ int	terminate(t_game *game)
 	free(game->draw.nswe);
 	exit(0);
 }
+
+t_door	*ft_lstnew(t_point pos, float dist)
+{
+	t_door	*res;
+
+	res = ft_calloc(1, sizeof(t_door));
+	if (!res)
+		return (NULL);
+	res->pos = pos;
+	res->dist = dist;
+	return (res);
+}
+
+void	ft_lstadd_front(t_door **alst, t_door *new)
+{
+	new->next = *alst;
+	*alst = new;
+}
+
